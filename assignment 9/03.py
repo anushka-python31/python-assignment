6ad18481176cb4497e6fa832a2734eda83c34e72
Anushka Nivangune <anushkanivangune51@gmail.com>
2026-01-10 19:08:04 +0530

    Delete assignment 9 directory

--- assignment 9/03.py
+++
@@ -1,7 +0,0 @@
-#wap to reverse a given number using recursive function.
-def reverse_number(n, rev=0): 
-    if n == 0:
-         return rev 
-    return reverse_number(n // 10, rev * 10 + n % 10) 
-num = int(input("Enter number: ")) 
-print("Reversed:", reverse_number(num))
