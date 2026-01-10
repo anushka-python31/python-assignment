e1c9d36746f4cc69383e9294c0cb327ce824776d
Anushka Nivangune <anushkanivangune51@gmail.com>
2026-01-10 19:06:11 +0530

    Delete assignment 2 directory

--- assignment 2/02.py
+++
@@ -1,9 +0,0 @@
-#2. Convert temp from Celsius to Fahrenheit. (C/5 = (F-32)/9) 
- 
-celcius = float(input('Enter the celcius:')) 
- 
-fahrehanti = (celcius * 9 ) / 5  +  35 
- 
-print(' Tempreture in fahrehanti = ', fahrehanti) 
- 
- 
