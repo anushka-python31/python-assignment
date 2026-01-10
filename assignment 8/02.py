683d58ec23626153f7effa6a556861bd45e0ab04
Anushka Nivangune <anushkanivangune51@gmail.com>
2026-01-10 19:07:30 +0530

    Delete assignment 8 directory

--- assignment 8/02.py
+++
@@ -1,6 +0,0 @@
-#wap to calculate circle using function
-def circle_area(radius):
-    return 2*3.14*radius
-radius=float(input("Enter radius of circle:"))
-area=circle_area(radius)
-print("the area of circle:",area)
--- assignment 8/03.py
+++
@@ -1,5 +0,0 @@
-#sum of all odd number between 1 to 10
-def sum_of_odd(n):
-    return sum(range(1,n+1,2))
-n=int(input("Enter a number"))
-print("sum of all odd number from 1 to 10:",n, "is:",sum_of_odd(n))
