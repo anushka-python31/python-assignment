26389e9fceaf09b8575dc89a786aaa154ce03072
Anushka Nivangune <anushkanivangune51@gmail.com>
2026-01-11 12:24:09 +0530

    Add files via upload

---
+++ assignment 08/01.py
@@ -0,0 +1,7 @@
+#wap to calculate area of rectangle ueing function
+def rectangle_area(length,breath):
+    return length*breath
+length=int(input("Enter the length of the rectangle:"))
+brath=int(input("Enter the brath of the rectangle:"))
+area = rectangle_area(len)
+
