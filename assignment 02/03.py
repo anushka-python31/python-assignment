040816ade263f346e0eade74c54b189d206f00b2
Anushka Nivangune <anushkanivangune51@gmail.com>
2026-01-11 12:17:33 +0530

    Add files via upload

---
+++ assignment 02/03.py
@@ -0,0 +1,7 @@
+#Convert distant given in feet and inches into meter and centimeter. 
+# 1foot = 30.48 
+# 1inches = 2.54 
+ 
+# Take input 
+feet = int(input('Enter the feet:')) 
+inches = int(input('Enter the inches:'))
